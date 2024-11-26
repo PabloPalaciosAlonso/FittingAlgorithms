be05974da50c5697818e055b7e37e58e5883fd8f
PabloPalaciosAlonso <pablo.pal@hotmail.com>
2024-11-26 22:35:16 +0100

    First version of the codes

---
+++ src/utils/defines.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define fori(x,y) for(int i=x; i<int(y); i++)
+#define forj(x,y) for(int j=x; j<int(y); j++)
+
---
+++ src/utils/randomNumbers.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<random>
+
+namespace parallelTempering {
+  
+  std::mt19937& getRng() {
+    static std::random_device rd;
+    static std::mt19937 rng(rd());
+    return rng;
+  }
+  
+  int generateRandomInt(int first, int last) {
+    std::uniform_int_distribution<int> dist(first, last);
+    return dist(getRng());
+  }
+  
+  double generateRandomDouble(double first, double last) {
+    std::uniform_real_distribution<double> dist(first, last);
+    return dist(getRng());
+  }
+}
