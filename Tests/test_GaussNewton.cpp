12fb378da6bfa7b81b7e26b0e6303fc60e9fb1b3
PabloPalaciosAlonso <pablo.pal@hotmail.com>
2025-11-18 14:22:29 +0100

    Add test for the jacobian calculation

--- Tests/test_GaussNewton.cpp
+++ Tests/test_GaussNewton.cpp
@@ -1,20 +1,68 @@
 #include <gtest/gtest.h>
 #include "GaussNewton.h"
+#include "costFunctions.h"
+
+
+namespace FittingAlgorithms{
+  namespace GaussNewton{
+
+Eigen::MatrixXd computeJacobian(std::vector<double>& xdata,
+                                std::vector<double>& ydata,
+                                ModelFunction model,
+                                CostFunction costFunction,
+                                StringDoubleMap& paramsMap,
+                                StringDoubleMap& extraParameters);
+  }
+}
+
+double polynomicModel(double x,
+                      const StringDoubleMap& fittingParams,
+                      const StringDoubleMap& extraParams) {
+  double a = fittingParams.at("a");
+  double b = fittingParams.at("b");
+  double c = fittingParams.at("c");
+  return a*x*x + b*x + c;
+};
 
 using namespace FittingAlgorithms;
 using namespace GaussNewton;
 
+TEST(GaussNewton, JacobianPolynomicModel){
+
+  StringDoubleMap employedParams = {{"a", 2.5}, {"b", -1.2},{"c", 0.5}};
+  StringDoubleMap extraParams    = {};
+  
+  std::vector<double> pointsX = {-3.654, -2.342, -0.75, 0.33, 1.3, 2.44};
+  std::vector<double> pointsY(pointsX.size());
+
+  int ncols = employedParams.size();
+  int nrows = pointsX.size();
+  
+  for(int i = 0; i<nrows; i++)
+    pointsY[i] = polynomicModel(pointsX[i], employedParams, extraParams);
+
+  std::vector<double> Jtheo(ncols*nrows);
+
+  for (int point = 0; point<nrows; point++){
+    Jtheo[ncols*point+0] = -pointsX[point]*pointsX[point];
+    Jtheo[ncols*point+1] = -pointsX[point];
+    Jtheo[ncols*point+2] = -1.0;
+  }
+
+  Eigen::MatrixXd Jnum = computeJacobian(pointsX, pointsY, polynomicModel,
+                                         squaredError, employedParams, extraParams);
+  
+  for (int row = 0; row<nrows; row++){
+    for (int col = 0; col<ncols; col++){
+      double Jij_theo = Jtheo[col + row*ncols];
+      double Jij_num  = Jnum(row, col);
+      EXPECT_NEAR(Jij_theo, Jij_num, 1e-6);
+    }
+  }
+}
+
 // Checks that Gauss-Newton correctly fits a second-degree polynomial
 TEST(GaussNewton, fitPolynomicFunction){
-  
-  ModelFunction polynomicModel = [](double x,
-                                    const StringDoubleMap& fittingParams,
-                                      const StringDoubleMap& extraParams) {
-    double a = fittingParams.at("a");
-    double b = fittingParams.at("b");
-    double c = fittingParams.at("c");
-    return a*x*x + b*x + c;
-  };
   
   std::vector<double> pointsX = {-1.3, -1.111, 0.0, 0.34, 0.76, 1.21, 2.3, 3};
   std::vector<double> pointsY(pointsX.size());
