260b6ea0f4996f015d767f53b6ab8b3f56d7503a
PabloPalaciosAlonso <pablo.pal@hotmail.com>
2026-04-20 14:03:21 +0200

    Fix bug

--- src/ParallelTempering_detail.h
+++ src/ParallelTempering_detail.h
@@ -190,13 +190,13 @@ namespace FittingAlgorithms{
       }
     }
 
-    template<class T1, class T2, class T3 = StringDoubleMap>
+    template<class T1, class T2, class T3 = StringDoubleMap&>
     double computeAverageError(const std::vector<T1>& xdata_in,
                                const std::vector<T2>& ydata_in,
                                const ModelFunction<T1, T2, T3>& model,
                                const StringDoubleMap& fittingParameters,
                                const CostFunction<T2>& costFunction,                               
-                               const StringDoubleMap& extraParameters) {
+                               const T3 extraParameters) {
       
       auto calculateError = [&](const T1& x_element, const T2& y_actual) {
         T2 y_pred = model(x_element, fittingParameters, extraParameters);
